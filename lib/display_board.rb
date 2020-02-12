719bb67a58c189c287da1a61a17cb28ff1892be4
sensible-draft-9261 <jasmine.williamson@me.com>
2020-02-12 18:21:21 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -9,7 +9,7 @@
    board=[" X "," "," "," "," X "," "," "," "," X "]
    board=[" "," "," O "," "," O "," "," O "," "," "]
    board=[" "," O "," "," "," X "," "," "," X "," "]
-   board=["X","X","X","X","X","X","X","X","X"]
+   board=[" X "," X "," X "," X "," X "," X "," X "," X "," X "]
    board=[" O "," O "," O "," O "," O "," O "," O "," O "," O "]
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
