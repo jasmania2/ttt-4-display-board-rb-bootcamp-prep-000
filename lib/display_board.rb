55c6045e1a1d916d2c94d539c9609eac320a197d
sensible-draft-9261 <jasmine.williamson@me.com>
2020-02-12 17:43:50 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -10,7 +10,7 @@
    board=[" "," "," O "," "," O "," "," O "," "," "]
    board=[" "," O "," "," "," X "," "," "," X "," "]
    board=[" X "," X "," X "," X "," X "," X "," X "," X "," X "]
-   board=[" O "," O "," O "]
+   board=[" O "," O "," O "," O "," O "," O "," O "," O "," O "]
 def display_board(board)
   puts "   |   |   "
   puts "-----------"
