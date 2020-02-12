451b831203cf7c87967a3e99479cccef3870a021
sensible-draft-9261 <jasmine.williamson@me.com>
2020-02-12 18:25:56 +0000

    Automatically backed up by Learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -165,7 +165,7 @@ describe "#display_board in 'lib/display_board.rb" do
 
       # *** Edit the lines below ***
       # *** Uncomment the lines below ***
-      expect(rows[0]).to eq(" X | X | X ")
+      #expect(rows[0]).to eq(" X | X | X ")
       expect(rows[1]).to eq("-----------")
       expect(rows[2]).to eq(" X | X | X ")
       expect(rows[3]).to eq("-----------")
